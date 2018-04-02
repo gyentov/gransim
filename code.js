d4992a3a958dc4b3abbf2bd6c7568495ba7c1d81
Genta <fgahunter@gmail.com>
2018-04-02 23:27:20 +0700

    Preparing environment

---
+++ code.js
@@ -0,0 +1,37 @@
+/*
+This is a simulation to model a boiling fluid.
+The model was based on granular physics.
+We use heat capacity and fire's wavelength as input parameter.
+
+Author :
+Feby Genta Ananda (10214003)
+Hessel Julius (10214029)
+Rizky Adhitama (10214077)
+Muhammad Aulia Reynaldi Nasution (10214095)
+
+Acknowledgement :
+We use some libraries from Mr. Sparisoma Viridi's repository.
+Link : https://github.com/dudung/butiran
+*/
+
+// HTML Setup
+
+// Textarea for input
+
+// Start/Stop Button
+
+// Textarea to Display Data
+
+// Simulation animation
+
+// Fluid's average energy to time plot
+
+// Functions
+
+// Funct. 1 to blablabla
+
+// Funct. 2 to blablabla
+
+// Funct. 3 to blablabla
+
+// Funct. 4 to blablabla
